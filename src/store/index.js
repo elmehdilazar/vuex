b63c1bf25052763415db6c40b6ef73ebb3b88b55
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-08-14 23:21:45 +0100

    add setup

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,34 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    count:0
+    count: 0,
+    books:[],
   },
   getters: {
   },
   mutations: {
     increment(state,number) {
       state.count += number;
+    },
+    fetchBook(state) {
+      const books = JSON.parse(localStorage.getItem("books"));
+      state.books = books;
+     
+    },
+    storeBook(state, book) {
+      state.books.push(book);
+      
     }
   },
   actions: {
+    getBooks(context) {
+      context.commit("fetchBook");
+     
+    },
+    //with parameter
+    addbook({commit},book) {
+      commit("storeBook", book);
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,28 +2,49 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-{{  count }} <button type="button" @click="increment">increment</button>
+    {{ count }} <button type="button" @click="increment">increment</button>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">{{ book.title }}</li>
+    </ul>
+    <button type="button" @click="addBook">add</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { useStore } from "vuex";
+import { computed,onMounted } from "vue";
 
 export default {
-  name: 'HomeView',
-  computed: {
-    count() {
-      return this.$store.state.count;
-    }
-  },
+  name: "HomeView",
   components: {
-    HelloWorld
+    HelloWorld,
   },
-  methods: {
-    increment() {
-      this.$store.commit("increment", 2);
+  setup() {
+    const store = useStore();
+    const count = computed(() => store.state.count);
+    const books = computed(() => store.state.books);
+    function increment() {
+      store.commit("increment", 2);
     }
+    function addBook() {
+      const book = {
+        ref: "aaayyyttt",
+        title: "vue js",
+        descripiotn: "ddddd kkkkkk ddddddddjjjjjjjj",
+      };
+      store.dispatch("addbook", book);
+    }
+    onMounted(() => {
+      store.dispatch("getBooks");
+    });
+    return {
+      count,
+      books,
+      increment,
+      addBook,
+    };
   },
-}
+};
 </script>
